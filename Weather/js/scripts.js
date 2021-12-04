3d364cde4b801775e7ad23f14deb7b97b4293b0b
glen-s-abraham <glenprojects666@gmail.com>
2021-12-04 11:34:58 +0530

    weather app completed

---
+++ Weather/js/scripts.js
@@ -0,0 +1,27 @@
+const appId="a7471dd4ca6874238fe2985188ddedcf".toString();
+const getCityName=()=>{
+    let cityName=document.querySelector("#cityName").value;
+    return cityName;
+}
+ const fetchData = async ()=>{
+    console.log("fetch data");
+    const q = getCityName();
+    if(q==""){
+        alert("Please Enter city Name");
+    }
+    //promises
+    // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${appId}`)
+    //     .then(response=>response.json())
+    //     .then(data=>console.log(data))
+    //     .catch(err=>console.log(err));
+
+    //async await
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${appId}&units=metric`);
+    const data = await response.json();
+    console.log(data);
+    document.querySelector("#temp").innerHTML=`${data.main.temp}<sup>0</sup>C`;
+    document.querySelector("#humidity").innerHTML=`${data.main.humidity}%`;
+    document.querySelector("#wind").innerHTML=`${data.wind.speed}Km/h`;
+}
+
+document.querySelector("#check").addEventListener("click",fetchData);
